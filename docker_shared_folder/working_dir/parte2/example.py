a1dc737c8782693bb602f0f03f943c8fc6378c53
svorsic <124444300+svorsic@users.noreply.github.com>
2023-07-11 23:23:35 -0300

    Nuevos cambios

--- docker_shared_folder/working_dir/parte2/example.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-url = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/cars_clus.csv'
-filename = 'cars_clus.csv'
-df=urllib.request.urlretrieve(url, filename)
-import pandas as pd
-pdf = pd.read_csv(filename)
-pdf
